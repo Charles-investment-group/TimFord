7ce5716c534dcb246f42bab89c680aebc2df38e0
ranger097 <ranger097@proton.me>
2026-08-04 15:14:22 -0400

    edited static.yml

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+const base = process.env.BASE_PATH || (process.env.NODE_ENV === 'production' ? '/cig' : '');
+
 const config = {
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production'
-				? '/cig'
-				: ''
+			base
 		}
 	}
 };
