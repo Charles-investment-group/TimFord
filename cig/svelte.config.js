c7b842d05b2a2e3df94a87f14fa919092000b63e
ranger097 <ranger097@proton.me>
2026-08-04 12:59:27 -0400

    changed static.yml

--- cig/svelte.config.js
+++ cig/svelte.config.js
@@ -1,2 +1,17 @@
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {}
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production'
+				? '/cig'
+				: ''
+		}
+	}
+};
+
+export default config;
