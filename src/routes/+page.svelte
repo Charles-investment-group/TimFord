0b4443dd71b852a8c228c1050b96eb43ed9d858e
Muhammad Junaid Asif <asifjunaid27@gmail.com>
2026-08-07 05:00:01 -0700

    Add ForgeEngine component to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 import Header from './Header.svelte';
 import Hero from './Hero.svelte';
+import ForgeEngine from './ForgeEngine.svelte';
 </script>
 
 <main>
 <Header />
 <Hero />
+<ForgeEngine />
 </main>
