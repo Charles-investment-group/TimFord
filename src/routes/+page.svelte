c852f07315e60e6a097929b11e85446d527fa9c9
Gideon Komla Agbavor <gideonagbavor8@gmail.com>
2026-08-08 02:08:10 +0000

    Build Section 3 landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Section3 from '$lib/Section3.svelte';
+</script>
+
+<Section3 />
