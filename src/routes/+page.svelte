a6f03fb1ed1d18c51169b1cc4be9fe9c562de24e
CaptainMarvel1 <marvellousadegbite336@gmail.com>
2026-09-25 13:44:15 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,16 @@
     import Footer from './Footer.svelte' 
 </script>
 
+<script>
+  export let data;
+</script>
+
+<ul>
+  {#each data.countries as country}
+    <li>{country.name}</li>
+  {/each}
+</ul>
+
 <main>
     <Header />
     <Hero  />
