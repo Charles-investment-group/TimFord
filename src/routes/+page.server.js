093835ab215a6e1e6bf039d933419789990b324c
CaptainMarvel1 <marvellousadegbite336@gmail.com>
2026-09-25 13:48:53 +0100

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,12 +2,6 @@ import { GoogleGenAI } from "@google/genai";
 import { GEMINI_API_KEY } from "$env/static/private";
 import { supabase } from "$lib/supabaseClient";
 
-export async function load() {
-  const { data } = await supabase.from("countries").select();
-  return {
-    countries: data ?? [],
-  };
-}
 
 export const prerender = false; 
 
@@ -41,7 +35,11 @@ export const actions = {
       return {
         aiResponse: `Failed to generate AI response. ${error.message || ""}`
       };
-    }
-  }
-};
+    } export async function load() {
+  const { data } = await supabase.from("countries").select();
+  return {
+    countries: data ?? [],
+  };
+}
+
 
